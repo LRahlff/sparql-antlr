aba49603bc3f7cfe3172e22f79974e4044a5ef84
Ludwig Rahlff <ludwig.rahlff@student.uni-luebeck.de>
2023-04-27 12:11:53 +0200

    refactored SQL instructions

--- main.py
+++ main.py
@@ -43,56 +43,86 @@ if __name__ == '__main__':
 
 	myTree.traverseVars()
 
-	sql_insert_value = lambda table, val_id, value : "INSERT INTO " + table +\
-											 " SELECT DISTINCT '" + val_id + "' AS new_val_id, " \
-																"p.param_id AS param_id " \
-												"FROM param p " \
-												"WHERE param_id LIKE '" + value + "'; "
-	sql_insert_material = lambda table, val_id, material: "INSERT INTO " + table +\
-											 " SELECT DISTINCT '" + val_id + "' AS new_val_id, " \
-																"m.mat_sample_id AS mat_sample_id " \
-												"FROM (SELECT sample_id AS mat_sample_id FROM sample " \
+	sql_insert_new_material = lambda materials: "INSERT INTO new_mat_samples " \
+													"SELECT DISTINCT new_mat.mat " \
+													"FROM (VALUES " + materials + ") AS new_mat (mat) " \
+														"LEFT OUTER JOIN  " \
+														"(	SELECT sample_id AS mat_sample_id " \
+															"FROM sample " \
+															"UNION SELECT mat_id AS mat_sample_id FROM material " \
+														") m " \
+															"ON m.mat_sample_id = new_mat.mat " \
+														"WHERE m.mat_sample_id IS NULL ; "
+
+	sql_insert_param_id = lambda new_params: "INSERT INTO new_param_id " +\
+												"SELECT DISTINCT " +\
+												"newval.new_val_id, " +\
+												"p.param_id " +\
+												"FROM  " +\
+													"(VALUES " + new_params + ") AS newval (new_val_id, param_id) " +\
+													"LEFT OUTER JOIN " +\
+													"param p " +\
+													"ON p.param_id = newval.param_id " +\
+												"WHERE p.param_id IS NOT NULL"
+
+	sql_insert_value = lambda new_values: "INSERT INTO new_value " +\
+												"SELECT DISTINCT " +\
+												"valid.new_val_id, " +\
+												"newval.value " +\
+												"FROM  " +\
+													"(VALUES " + new_values + ") AS newval (new_val_id, value) " +\
+													"LEFT OUTER JOIN " +\
+													"new_param_id valid " +\
+													"ON valid.new_val_id = newval.new_val_id " +\
+												"WHERE valid.new_val_id IS NOT NULL"
+
+	sql_insert_material = lambda new_material: "INSERT INTO new_mat_sample_id " +\
+												"SELECT DISTINCT " +\
+												"valid.new_val_id, " +\
+												"m.mat_sample_id " +\
+												"FROM  " +\
+													"(VALUES " + new_material + ") AS newmat (new_val_id, material) " +\
+													"LEFT OUTER JOIN  " \
+													"(SELECT sample_id AS mat_sample_id FROM sample " \
 														"UNION SELECT mat_id AS mat_sample_id FROM material " \
 														"UNION SELECT mat_sample_id AS mat_sample_id FROM new_mat_samples " \
-														") m " \
-												"WHERE mat_sample_id LIKE '" + material + "'; "
-	sql_insert_new_material = lambda table, new_material: "INSERT INTO " + table +\
-											 " SELECT DISTINCT new_mat.mat FROM " \
-												"(VALUES ('" + new_material+ "')) AS new_mat (mat) " \
-												"LEFT OUTER JOIN  " \
-												"(	SELECT sample_id AS mat_sample_id " \
-													"FROM sample " \
-													"UNION SELECT mat_id AS mat_sample_id FROM material " \
-												") m " \
-												"ON m.mat_sample_id = new_mat.mat " \
-												"WHERE m.mat_sample_id IS NULL ; "
+													") m " \
+													"ON m.mat_sample_id = newmat.material " +\
+													"LEFT OUTER JOIN " +\
+														"new_param_id valid " +\
+													"ON valid.new_val_id = newmat.new_val_id " +\
+												"WHERE m.mat_sample_id IS NOT NULL AND valid.new_val_id IS NOT NULL"
 
 	new_mat = myTree.get_new_materials()
 
 	# Todo : better way, maybe with lamda function
-	for m in new_mat:
-		sqlrequest += sql_insert_new_material("new_mat_samples", m)
+	if len(new_mat)>0:
+		materials = "('" + "'), ('".join(new_mat) + "')"
+		sqlrequest += sql_insert_new_material(materials)
 
 	new_values = myTree.get_new_params()
 
 	tree = myTree.forward
 	rev_tree = myTree.reverse
+	params = []
+	values = []
+	material = []
 	for new_value in new_values:
 		if tree.get(new_value) is not None:
 			if tree[new_value].get(A_NODE) is not None:
 				for obj in tree[new_value][A_NODE]:
 					if obj.type == 'string':
-						sqlrequest += sql_insert_value('new_param_id', new_value.name, obj.name)
+						params.append("('" + new_value.name + "', '" + obj.name + "')")
 					if obj.type == 'konzept':
 						if koncept_translation.get(obj.name) is not None:
-							sqlrequest += sql_insert_value('new_param_id', new_value.name, koncept_translation[obj.name])
+							params.append("('" + new_value.name + "', '" + koncept_translation[obj.name] + "')")
 						else:
-							sqlrequest += sql_insert_value('new_param_id', new_value.name, obj.name)
+							params.append("('" + new_value.name + "', '" + obj.name + "')")
 
 			if tree[new_value].get(HAT_WERT_NODE) is not None:
 				for obj in tree[new_value][HAT_WERT_NODE]:
 					if obj.type == 'numeric':
-						sqlrequest += "INSERT INTO new_value VALUES ('" + new_value.name + "', " + obj.name + ") ; "
+						values.append("('" + new_value.name + "', " + obj.name + ")")
 
 		if rev_tree.get(new_value) is not None:
 			if rev_tree[new_value].get(HAT_PARAMETER_NODE) is not None:
@@ -101,5 +131,8 @@ if __name__ == '__main__':
 						if tree[subrev].get(HAT_NAME_NODE) is not None:
 							for obj2 in tree[subrev][HAT_NAME_NODE]:
 								if obj2.type == 'string' or obj2.type == 'konzept':
-									sqlrequest += sql_insert_material('new_mat_sample_id', new_value.name, obj2.name)
+									material.append("('" + new_value.name + "', '" + obj2.name + "')")
+	sqlrequest += sql_insert_param_id(", ".join(params)) + " ; "
+	sqlrequest += sql_insert_value(", ".join(values)) + " ; "
+	sqlrequest += sql_insert_material(", ".join(material)) + " ; "
 	print(sqlrequest)
